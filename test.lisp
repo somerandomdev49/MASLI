8fbdd25e9445b517069a879c5b146d472ece7ba9
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2020-04-16 08:42:42 +0300

    what the heck

--- test.lisp
+++ test.lisp
@@ -1,34 +1,5 @@
 (&
-<<<<<<< HEAD
-	(import test2)
-	(decl abcde (Num Any))
-	(defun
-		abcde
-		(a b)
-		(&
-			(if (> a b)
-				(write "! " a " is greater than " b)
-				(+ a b)
-			)
-		)
-	)
-	(abcde
-		123
-		2
-	)
-	(list -1.2 2 3 4)
-	abcde
-	"Hello, World!"
-	(map (a 1) (x (map (a 1))) (b (abcde 0 2)) (c 3))
-	abcde 1 2
-	(let Num x (abcde 1 3))
-	x
-	(= x 5)
-	x
-	(meme 4 3)
-=======
 	(import stdlib core core)
 	(import stdlib math strictcore) ; strict math core, only numbers are allowed.
 	(+ 1 1)
->>>>>>> d3ebccd04fbd100c53c47fcca44a530f678b5684
 )
